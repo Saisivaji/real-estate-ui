c88449514fdfc1c6f01ae5b775f9ca3669df63b6
SivajiB <boddusaisivaji2021@gmail.com>
2024-05-30 21:24:47 -0500

    implemented NavBar

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,18 @@
+import Navbar from "./components/navbar/Navbar";
+import HomePage from "./routes/homepage/HomePage";
+import "./layout.scss";
+
 function App() {
   return (
-    <div>Hello World</div>
-  )
+    <div className="layout">
+      <div className="navbar">
+        <Navbar />
+      </div>
+      <div className="content">
+        <HomePage />
+      </div>
+    </div>
+  );
 }
 
-export default App
+export default App;
---
+++ src/routes/homepage/HomePage.jsx
@@ -0,0 +1,18 @@
+import "./homePage.scss";
+export default function HomePage() {
+  return (
+    <div className="homePage">
+      <div className="textContainer">
+        <div className="wrapper">
+          <div className="title">
+            <h1>Find Real Estate & Get Your Dream Place </h1>
+            <p></p>
+          </div>
+        </div>
+      </div>
+      <div className="imageContainer">
+        <img src="/bg.png" alt="" />
+      </div>
+    </div>
+  );
+}
