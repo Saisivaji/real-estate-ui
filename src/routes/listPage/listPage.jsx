d3fff744bc959525ffbef70b776103de51773da0
SivajiB <boddusaisivaji2021@gmail.com>
2024-06-16 13:25:46 -0500

    implemented Card Component

--- src/routes/listPage/listPage.jsx
+++ src/routes/listPage/listPage.jsx
@@ -1,13 +1,19 @@
 import { listData } from "../../libraries/dummyData";
 import "../../routes/listPage/listPage.scss";
-import Filter  from "../../components/filter/filter";
+import Filter from "../../components/filter/filter";
+import Card from "../../components/card/card";
 
 export default function ListPage() {
   const data = listData;
   return (
     <div className="listPage">
       <div className="listContainer">
-        <div className="wrapper"><Filter/></div>
+        <div className="wrapper">
+          <Filter />
+          {data.map((item) => (
+            <Card key={item.id} item={item} />
+          ))}
+        </div>
       </div>
       <div className="mapContainer">Map</div>
     </div>
